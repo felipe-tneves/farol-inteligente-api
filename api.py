3d3f1ead148b73bf5b21c82c3b35828ee440e82a
Victor Aro <victor.galves@live.com>
2022-10-17 01:47:26 -0300

    .

--- api.py
+++ api.py
@@ -2,63 +2,69 @@ from ast import For
 from cgitb import reset
 from queue import Empty
 from unittest import result
-from flask import Flask, Response, request,  jsonify, send_from_directory
+from flask import Flask, Response, request, jsonify, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
-import mysql.connector 
+import mysql.connector
 import numpy as np
 import json
 import torch
 import datetime
 import requests
-import os 
+import os
 
-#Diretorio onde vai ficar os arquivos de upload 
-DIRETORIO = "C:\\Users\\Workspace\\Desktop\\TCC\\farol_inteligente\\files\\"
-
+#Diretorio onde vai ficar os arquivos de upload
+DIRETORIO = "C:\\Users\\Workspace\\Desktop\\Projects\\farol-inteligente-api\\files\\"
 #Conexão com o banco de dados
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:84268426aS$@localhost/tcc'
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] = 'mysql://root:84268426aS$@localhost/tcc'
 
 db = SQLAlchemy(app)
 
+
 #Model da tabela transito
 class Transito(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     qtd = db.Column(db.Integer)
     data = db.Column(db.String(100))
     hora = db.Column(db.String(100))
     clima = db.Column(db.String(300))
     endereco = db.Column(db.String(200))
 
-
     def to_json(self):
-        return {"id": self.id, "qtd": self.qtd, "data": self.data,  "hora": self.hora, "clima": self.clima, "endereco": self.endereco}
+        return {
+            "id": self.id,
+            "qtd": self.qtd,
+            "data": self.data,
+            "hora": self.hora,
+            "clima": self.clima,
+            "endereco": self.endereco
+        }
 
 
 #yolo com a quantidade carros
 def yolo(nome_arquivo):
     model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 
-
     print(nome_arquivo)
     imgs = DIRETORIO + nome_arquivo
 
-    
     results = model(imgs)
     results.save()
     print(results)
     result_car = results.pandas().xyxy[0]
 
-    df = pd.DataFrame(result_car, columns = ["confidence", "name"])
+    df = pd.DataFrame(result_car, columns=["confidence", "name"])
     detected = df['name'].str.contains('bus|car')
-    #pega a quantidade de elementos de um dataframe 
+    #pega a quantidade de elementos de um dataframe
     i = 0
-    for object in detected: 
-        if object:   
+    for object in detected:
+        if object:
             i = i + 1
     return i
+
 
 #função para pegar a data e hora
 def time_now():
@@ -67,15 +73,18 @@ def time_now():
 
     return [hora, data]
 
+
 #função para pegar o clima tempo
 def climaTempo():
     token = "ee369bc12a04e11edc6af6b841af96e1"
-    clima_url = "http://apiadvisor.climatempo.com.br/api/v1/weather/locale/3477/current?token=" + str(token)
+    clima_url = "http://apiadvisor.climatempo.com.br/api/v1/weather/locale/3477/current?token=" + str(
+        token)
     clima_response = requests.request("GET", clima_url)
     clima_retorno_req = json.loads(clima_response.text)
     data_clima = clima_retorno_req['data']
-   
+
     return data_clima
+
 
 #CRUD - GET
 @app.route("/transito", methods=["GET"])
@@ -88,11 +97,12 @@ def seleciona_tudo():
     except Exception as e:
         print(e)
 
+
 #CRUD - GET by ID
 @app.route("/transito/<id>", methods=["GET"])
 def seleciona_um(id):
     try:
-        transito_objeto = Transito.query.filter_by(id = id).first()
+        transito_objeto = Transito.query.filter_by(id=id).first()
         transito_json = transito_objeto.to_json()
         return transito_json
     except Exception as e:
@@ -105,16 +115,22 @@ def cadastro_test():
     body = request.get_json()
     try:
 
-        transito = Transito(qtd=body["qtd"], data=body["data"], hora=body["hora"], clima=body["clima"], endereco=body["endereco"])
+        transito = Transito(qtd=body["qtd"],
+                            data=body["data"],
+                            hora=body["hora"],
+                            clima=body["clima"],
+                            endereco=body["endereco"])
 
         db.session.add(transito)
         db.session.commit()
 
-        return gera_response(201, "transito", transito.to_json(), "Criado com sucesso")
-        
+        return gera_response(201, "transito", transito.to_json(),
+                             "Criado com sucesso")
+
     except Exception as e:
         print(e)
         return gera_response(400, "transito", {}, "Erro ao cadastrar")
+
 
 #CRUD - UPDATE
 @app.route("/transito/<id>", methods=["PUT"])
@@ -123,20 +139,21 @@ def atualiza(id):
     body = request.get_json()
 
     try:
-        if('qtd' in body):
+        if ('qtd' in body):
             transito_objeto.qtd = body['qtd']
-        if('data' in body):
+        if ('data' in body):
             transito_objeto.data = body['data']
-        if('hora' in body):
+        if ('hora' in body):
             transito_objeto.hora = body['hora']
-        if('clima' in body):
+        if ('clima' in body):
             transito_objeto.clima = body['clima']
-        if('endero' in body):
+        if ('endero' in body):
             transito_objeto.endero = body['endero']
-        
+
         db.session.add(transito_objeto)
         db.session.commit()
-        return gera_response(200, "transito", transito_objeto.to_json(), "Atualizado com sucesso")
+        return gera_response(200, "transito", transito_objeto.to_json(),
+                             "Atualizado com sucesso")
 
     except Exception as e:
         print(e)
@@ -151,55 +168,62 @@ def deleta(id):
     try:
         db.session.delete(transito_objeto)
         db.session.commit()
-        return gera_response(200, "transito", transito_objeto.to_json(), "Excluido com sucesso")
-    
+        return gera_response(200, "transito", transito_objeto.to_json(),
+                             "Excluido com sucesso")
+
     except Exception as e:
         print(e)
         return gera_response(400, "transito", {}, "Erro ao deletar")
 
 
-#lista os arquivos 
+#lista os arquivos
 @app.route("/arquivos", methods=["GET"])
 def lista_arquivos():
     arquivos = []
 
     for nome_do_arquivo in os.listdir(DIRETORIO):
         endereco_do_arquivo = os.path.join(DIRETORIO, nome_do_arquivo)
-        
-        if(os.path.isfile(endereco_do_arquivo)):
-            arquivos.append(nome_do_arquivo)
 
+        if (os.path.isfile(endereco_do_arquivo)):
+            arquivos.append(nome_do_arquivo)
 
     return jsonify(arquivos)
 
-#dowloads dos arquivos 
+
+#dowloads dos arquivos
 @app.route("/arquivos/<nome_do_arquivo>", methods=["GET"])
 def get_arquivo(nome_do_arquivo):
     return send_from_directory(DIRETORIO, nome_do_arquivo, as_attachment=True)
 
+
 #upload de arquivo
 @app.route("/arquivos", methods=["POST"])
 def post_arquivo():
-    
+
     arquivo = request.files.get("meuArquivo")
     endereco = request.form["rua"]
 
     nome_do_arquivo = arquivo.filename
     arquivo.save(os.path.join(DIRETORIO, nome_do_arquivo))
-    
+
     qtd = yolo(nome_do_arquivo)
     data_hora = time_now()
     clima_tempo = climaTempo()
-    
+
     try:
 
-        transito = Transito(qtd=int(qtd), data=data_hora[1], hora=data_hora[0], clima=str(clima_tempo), endereco=endereco)
+        transito = Transito(qtd=int(qtd),
+                            data=data_hora[1],
+                            hora=data_hora[0],
+                            clima=str(clima_tempo),
+                            endereco=endereco)
 
         db.session.add(transito)
         db.session.commit()
 
-        return gera_response(201, "transito", transito.to_json(), "Criado com sucesso")
-        
+        return gera_response(201, "transito", transito.to_json(),
+                             "Criado com sucesso")
+
     except Exception as e:
         print(e)
         return gera_response(400, "transito", {}, "Erro ao cadastrar")
@@ -209,10 +233,13 @@ def gera_response(status, nome_do_conteudo, conteudo, mensagem=False):
     body = {}
     body[nome_do_conteudo] = conteudo
 
-    if(mensagem):
+    if (mensagem):
         body["mensagem"] = mensagem
-    
-    return Response(json.dumps(body), status = status, mimetype="application/json")
+
+    return Response(json.dumps(body),
+                    status=status,
+                    mimetype="application/json")
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
